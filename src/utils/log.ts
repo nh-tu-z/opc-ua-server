43cae0d15be57b0186845cd6657cb73a623edf3e
Tu Ngo <nhtu1611@gmail.com>
2023-01-06 00:03:18 +0700

    init mqtt client cli

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,5 +1,5 @@
-import fs from 'fs'
-import chalk from 'chalk'
+import fs from 'fs';
+import chalk from 'chalk';
 
 const logToFile = (msg: string, instance: string) => {
     let date = new Date()
@@ -12,7 +12,7 @@ interface LoggerInterface {
     green(msg:String): void
     yellow(msg:String): void
     red(msg:String): void
-}
+};
 
 class Logger implements LoggerInterface {
 
@@ -22,19 +22,19 @@ class Logger implements LoggerInterface {
 
     green(msg : string, instance: string = 'LOG'): void {
         logToFile(msg, instance)
-        console.log(` ${instance}: `, chalk.green(msg))
+        console.log(` ${instance}: `, chalk.green(msg));
     };
 
     yellow (msg : string, instance: string = 'LOG'): void {
         logToFile(msg, instance)
-        console.log(` ${instance}: `, chalk.yellow(msg))
+        console.log(` ${instance}: `, chalk.yellow(msg));
     };
 
     red (msg : string, instance: string = 'LOG'): void {
         logToFile(msg, instance)
-        console.log(` ${instance}: `, chalk.red(msg))
+        console.log(` ${instance}: `, chalk.red(msg));
     };
-}
+};
 
 export const logger = new Logger();
 export const green = logger.green;
